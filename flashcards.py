32089a88630d3578d3f4eeb695ee8dbcb897b2d3
Drew Mercer <dmercer@redhat.com>
2023-04-06 14:29:28 -0400

    Added a simulated db layer

--- flashcards.py
+++ flashcards.py
@@ -3,16 +3,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+counter = 0
+
 @app.route("/")
 def welcome():
-    return render_template("welcome.html")
+    global counter
+    counter += 1
+    return render_template("welcome.html",
+                           message="This page has been served " + str(counter) + " times.")
 
 
 @app.route("/date")
 def date():
     return "This page was served at " + str(datetime.now())
 
-counter = 0
 
 @app.route("/count_views")
 def count_views():
