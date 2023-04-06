e595e9f7727fb4201d8690cc352d3a0a0322cdb3
Drew Mercer <dmercer@redhat.com>
2023-04-06 11:01:50 -0400

    Added two more routes

--- flashcards.py
+++ flashcards.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Flask
 app = Flask(__name__)
 
@@ -5,3 +6,16 @@ app = Flask(__name__)
 @app.route("/")
 def welcome():
     return "Welcome to my Flash Cards application!"
+
+
+@app.route("/date")
+def date():
+    return "This page was served at " + str(datetime.now())
+
+counter = 0
+
+@app.route("/count_views")
+def count_views():
+    global counter
+    counter += 1
+    return "This page has been served " + str(counter) + " times."
