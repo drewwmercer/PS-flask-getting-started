036d33143807c18183b6827dcb2b212e80b303eb
Drew Mercer <dmercer@redhat.com>
2023-04-10 08:01:07 -0400

    Added logic to card movement

--- flashcards.py
+++ flashcards.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 from model import db
 
 app = Flask(__name__)
@@ -30,5 +30,8 @@ def count_views():
 
 @app.route("/card/<int:index>")
 def card_view(index):
-    card = db[index]
-    return render_template("card.html", card=card)
+    try:
+        card = db[index]
+        return render_template("card.html", card=card, index=index)
+    except IndexError:
+        abort(404)
